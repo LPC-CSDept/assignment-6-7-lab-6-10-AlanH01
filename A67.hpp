319f867e924c486a6f4e13643a20cf6ae09f8e2c
AlanH01 <111603208+AlanH01@users.noreply.github.com>
2022-10-09 22:52:29 +0000

    Deleted int main

--- A67.hpp
+++ A67.hpp
@@ -21,34 +21,3 @@ void swap (int &a, int &b, int &c, int &d)
   c = d;
   d = t;
 }
-int main ()
-{
-  int n1, n2, n3, n4;
-  int max, min;
-  
-  n1 = 10;
-  n2 = 20;
-  swap (n1, n2);
-  cout << n1 << endl;
-  cout << n2 << endl;
-  
-  n1 = 20;
-  n2 = 30;
-  n3 = 10;
-  swap (n1, n2, n3);
-  cout << n1 << endl;
-  cout << n2 << endl;
-  cout << n3 << endl;
-
-  
-  n1 = 10;
-  n2 = 20;
-  n3 = 30;
-  n4 = 40;
-  swap (n1, n2, n3, n4);
-  cout << n1 << endl;
-  cout << n2 << endl;
-  cout << n3 << endl;
-  cout << n4 << endl;
-  return 0;
-}
